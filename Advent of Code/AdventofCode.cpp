b12673c9c71441f6a8d4fb5b47f21237bf9d8b33
Steven Bartels <steven.bartels@icloud.com>
2021-12-03 12:18:40 +0100

    AoC3

--- Advent of Code/AdventofCode.cpp
+++ Advent of Code/AdventofCode.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -81,14 +82,134 @@ void AoCTwo() {
 	}
 	cout << "AOC 02" << endl;
 	cout << "Positions: " << position[0] << " " << position[1] << " " << position[2] << endl;
-	cout << "Multiply: " << position[0] * position[1] << endl;
+	cout << "Multiply: " << position[0] * position[1] << endl << endl;
 }
 
 #pragma endregion AOC_02
 
+#pragma region AOC_03
+
+void ParseChar(int &position, string &line, int(&ones)[12], int(&zeros)[12]) {
+	if (line.c_str()[position] == '0') {
+		zeros[position] += 1;
+	}
+	else {
+		ones[position] += 1;
+	}
+}
+
+void ParseFileToGammaEpsilon(ifstream& file, int(&gamma)[12], int(&epsilon)[12]) {
+	string line;
+	int ones[12] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+	int zeros[12] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+	while (std::getline(file, line)) {
+		for (int i = 0; i < 12; ++i) {
+			ParseChar(i,line,ones,zeros);
+		}
+	}
+	for (int i = 0; i < 12; ++i) {
+		gamma[i] = (ones[i] > zeros[i] ? 1 : 0);
+		epsilon[i] = (ones[i] > zeros[i] ? 0 : 1);
+	}
+}
+
+void FindMatchingOnPosition(std::vector<string>& Lines, const int position, bool IsOxygen) {
+	if (position == 12) {
+		return;
+	}
+
+	int numOnes = 0;
+	int numZeros = 0;
+	for (string tmpLine : Lines) {
+		if (tmpLine.c_str()[position] == '1') {
+			numOnes += 1;
+		}
+		else {
+			numZeros += 1;
+		}
+	}
+
+	vector<string>::iterator it = Lines.begin();
+	while (it != Lines.end() && Lines.size() > 1) {
+		if (IsOxygen) {
+			if ((numOnes >= numZeros && (*it).c_str()[position] == '1')) {
+				++it;
+			}
+			else if ((numOnes < numZeros && (*it).c_str()[position] == '0')) {
+				++it;
+			}
+			else it = Lines.erase(it);
+		}
+		else {
+			if ((numOnes < numZeros && (*it).c_str()[position] == '1')) {
+				++it;
+			}
+			else if ((numOnes >= numZeros && (*it).c_str()[position] == '0')) {
+				++it;
+			}
+			else it = Lines.erase(it);
+		}
+	}
+
+	FindMatchingOnPosition(Lines, position+1, IsOxygen);
+}
+
+void ParseFileToOxygen(ifstream& file, string &OxygenRating, string &CO2Rating) {
+	std::vector<string> OxygenLines;
+	std::vector<string> CO2Lines;
+	string line;
+	while (std::getline(file, line)) {
+		OxygenLines.push_back(line);
+		CO2Lines.push_back(line);
+	}
+	FindMatchingOnPosition(OxygenLines, 0, true);
+	FindMatchingOnPosition(CO2Lines, 0, false);
+
+	OxygenRating = OxygenLines[0];
+	CO2Rating = CO2Lines[0];
+}
+
+int BinaryIntArrayToString(int(&arr)[12]) {
+	std::ostringstream os;
+	for (int i : arr) {
+		os << i;
+	}
+	std::string str(os.str());
+	return std::stoi(str, nullptr, 2);
+}
+
+void AoCThree() {
+	int gamma[12] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+	int epsilon[12] = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+	string OxygenRating, CO2Rating;
+	std::ifstream infile("InputFiles/inputAoC_03.txt");
+	if (infile.is_open()) {
+		ParseFileToGammaEpsilon(infile, gamma, epsilon);
+		infile.clear();
+		infile.seekg(0);
+		ParseFileToOxygen(infile, OxygenRating, CO2Rating);
+		infile.close();
+	}
+
+	int gammaNumber = BinaryIntArrayToString(gamma);
+	int epsilonNumber = BinaryIntArrayToString(epsilon);
+	int oxygenRatingNumber = std::stoi(OxygenRating, nullptr, 2);
+	int co2RatingNumber = std::stoi(CO2Rating, nullptr, 2);
+
+	cout << "AOC 03" << endl;
+	cout << "Gamma: " << gammaNumber << " Epsilon: " << epsilonNumber << endl;
+	cout << "O2: " << oxygenRatingNumber << " CO2: " << co2RatingNumber << endl;
+	cout << "Power Consumption: " << gammaNumber * epsilonNumber << endl;
+	cout << "LifeSupport Rating: " << oxygenRatingNumber * co2RatingNumber << endl;
+
+}
+
+#pragma endregion AOC_03
+
 int main() {
 	AoCOne();
 	AoCTwo();
+	AoCThree();
 	cin.get();
 	return 0;
 }
